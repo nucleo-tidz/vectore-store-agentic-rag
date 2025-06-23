bcc209fe743b91ada6e4bc4fbdb912a5a5536734
ahmar-husain <ahmar.husain@maersk.com>
2025-06-23 10:21:03 +0530

    Chcked

--- src/infrastructure/DependencyInjection.cs
+++ src/infrastructure/DependencyInjection.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace infrastructure
 {
+    [Experimental("SEMX")]
     public static class DependencyInjection
     {
-        public static IServiceCollection Add(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddSemanticKernel(this IServiceCollection services, IConfiguration configuration)
         {
-            return services;
-        }
+            return services.AddTransient<Kernel>(serviceProvider =>
+            {
+                IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+                kernelBuilder.AddAzureOpenAIEmbeddingGenerator
+                (deploymentName: "text-embedding-3-large", endpoint: configuration["foundry-endpoint-embedder"],
+                apiKey: configuration["apikey-embedder"]);
 
+                kernelBuilder.Services.AddAzureOpenAIChatCompletion("o4-mini",
+                  configuration["foundry-endpoint-mini"],
+                  configuration["apikey-mini"],
+                   "o4-mini",
+                   "o4-mini")
+                .AddRedisVectorStore("");
+                return kernelBuilder.Build();
+            });
+        }
     }
 }
