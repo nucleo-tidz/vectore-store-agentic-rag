e3b426fed92ee8454f9f41f888c8d538fe39ed51
Ahmar Husain <ahmar.husain1@gmail.com>
2025-08-02 13:57:51 +0530

    Agent calculation (#3)
    * Calculation
    
    * Fix
    
    * Remove
    
    * better
    
    * fix
    
    ---------
    
    Co-authored-by: ahmar husain <nucleotidz@hotmail.com>

--- src/api/Controllers/ShipmentController.cs
+++ src/api/Controllers/ShipmentController.cs
@@ -10,14 +10,14 @@ namespace api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController( Kernel kernel, IProjectAgent projectAgent) : ControllerBase
+    public class ShipmentController( Kernel kernel, IProjectAgent projectAgent) : ControllerBase
     {
        
         [HttpGet("chat/{message}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Chat(string message)
         {
-            //A container scheduled for dispatch has a gross weight of 9500 kg and a volume of 150.3 CBM. It is designated for a FuelSensitive route and requires special handling due to the presence of hazardous materials. Please calculate the total shipping cost using a base rate of ₹1001 per CBM.
+            //A container scheduled for dispatch has a gross weight of 9500 kg and a volume of 28.3 CBM. It is designated for a FuelSensitive route and requires special handling due to the presence of hazardous materials. Please calculate the total shipping cost using a base rate of ₹1001 per CBM.
             var response = await projectAgent.Execute(message);
             return Ok(response);
         }
